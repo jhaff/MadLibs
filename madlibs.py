00088ba468566f7219c3c103ed7ed3ca3c0f4d79
jhaff <jacobhaff2@gmail.com>
2018-08-30 14:30:40 -0700

    initial commit

---
+++ madlibs.py
@@ -0,0 +1,14 @@
+##This program acts as a madLib program asking the user for input and constructing
+##a story out of said inputs
+
+#gather words
+adjective1 = raw_input("Tell me an adjective, and click enter. ")
+noun1 = raw_input("Tell me a noun (plural), and click enter. ")
+noun2 = raw_input("Tell me another noun, and click enter. ")
+adjective2 = raw_input("Tell me an another adjective, and click enter. ")
+
+#construct story
+print "Vehicles are " + adjective1
+print noun1 + " are Orange"
+print noun2 + " is " + adjective2
+print "And so am I!"
