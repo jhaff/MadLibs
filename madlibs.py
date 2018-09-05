ffd06115c8cf7df6bee734c0075709a1c8a0d705
jhaff <jacobhaff2@gmail.com>
2018-09-05 14:28:47 -0700

    added new story, patrick, utilizing lists and numbers

--- madlibs.py
+++ madlibs.py
@@ -15,11 +15,20 @@ def spongebob():
     tup1= (adj1,adj2)
     print("Spongebob is %s and %s" % (colored(tup1[0], "yellow"), colored(tup1[1], "yellow")))
 
+def patrick():
+    adj1 = input("Enter an adjective: ")
+    noun = input("Enter a noun: ")
+    num = input("Enter a number ")
+    li= [adj1,noun,num]
+    print("Patrick had a %s %s which he sold for %s doubloons" % (colored(li[0], "yellow"), colored(li[1], "yellow"), colored(int(li[2]), "yellow")))
+
 #construct story
-story = input("Which story would you like? Options: \033[92msquidward, spongebob \033[37m")
+story = input("Which story would you like? Options: \033[92msquidward, spongebob, patrick \033[37m")
 if story == "squidward":
     squidward()
 elif story == "spongebob":
     spongebob()
+elif story == "patrick":
+    patrick()
 else:
     print("Doesn't Exist.")
