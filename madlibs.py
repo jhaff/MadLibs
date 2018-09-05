f3629a82dfb1ca79e813390150fc86594796ab95
jhaff <jacobhaff2@gmail.com>
2018-09-05 11:58:18 -0700

    Changed code to work in python3, changed story. Added story selector method.

--- madlibs.py
+++ madlibs.py
@@ -1,14 +1,16 @@
 ##This program acts as a madLib program asking the user for input and constructing
 ##a story out of said inputs
 
-#gather words
-adjective1 = raw_input("Tell me an adjective, and click enter. ")
-noun1 = raw_input("Tell me a noun (plural), and click enter. ")
-noun2 = raw_input("Tell me another noun, and click enter. ")
-adjective2 = raw_input("Tell me an another adjective, and click enter. ")
+#gather words for Squidward story
+def squidward():
+    verb = input("Enter a verb: ")
+    noun = input("Enter a noun: ")
+    anothernoun = input("Enter another noun: ")
+    print("Squidward %sed %s but the %s was utterly surprised by it." % (verb, noun, anothernoun))
 
 #construct story
-print "Vehicles are " + adjective1
-print noun1 + " are Orange"
-print noun2 + " is " + adjective2
-print "And so am I!"
+story = input("Which story would you like? Options: squidward  ")
+if story == "squidward":
+    squidward()
+else:
+    print("Doesn't Exist.")
